045f8a27d6ae47f8d4ba67ee629a8a7ffaa1e74c
haidi <haidinurhadinata@gmail.com>
2019-01-10 14:51:44 +0700

    update-dashboard-heroku

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,9 +50,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            // 'url' => env('APP_URL').'/storage',
-            'url' => env('APP_URL').'/public_html/public/storage',
+            'root' => storage_path('app/public/storages'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
 
