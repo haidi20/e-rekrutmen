6ee96406047e0d326d9d7436212b88c121737b25
haidi <haidinurhadinata@gmail.com>
2019-01-10 15:56:31 +0700

    update-dashboard-heroku

--- resources/views/dashboard/detail.blade.php
+++ resources/views/dashboard/detail.blade.php
@@ -54,7 +54,7 @@
         		<div align="center">
         			<div class="form-group">
                         <label for="foto">Foto</label><br>
-                        <img src="{{URL::asset('storages/'.$lowongan->gambar)}}" width="200">
+                        <img src="{{asset('storages')}}/{{$lowongan->gambar}}" width="200">
                     </div>
         		</div>
         		<h5 align="center">
